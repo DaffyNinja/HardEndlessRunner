bc502edc24d345c0142cc31e2bca89b2dbbd3f76
DaffyNinja <marcusday117@gmail.com>
2017-03-06 15:38:06 +1100

    Created animations for main menu buttons

--- HardEndlessRunner/Assets/Scripts/MainMenu.cs
+++ HardEndlessRunner/Assets/Scripts/MainMenu.cs
@@ -3,21 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour {
+public class MainMenu : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public bool doAnimation;
+    [Space(5)]
+    public Animator playTracksButton;
+    public Animator specialTracksButton;
+    public Animator settingsTracksButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        specialTracksButton.SetBool("isHidden", false);
+        playTracksButton.SetBool("isHidden", false);
+        settingsTracksButton.SetBool("isHidden", false);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     public void PlayButton()
     {
-        SceneManager.LoadScene(1); 
+        SceneManager.LoadScene(1);
+    }
+
+    public void SpecialTracksButton()
+    {
+        specialTracksButton.SetBool("isHidden", true);
+        playTracksButton.SetBool("isHidden", true);
+        settingsTracksButton.SetBool("isHidden", true);
+    }
+
+    public void SettingsButton()
+    {
+        specialTracksButton.SetBool("isHidden", true);
+        playTracksButton.SetBool("isHidden", true);
+        settingsTracksButton.SetBool("isHidden", true);
     }
 }
 
