4851589c6ab87fb690b7c90a3d3909b22967cc65
DaffyNinja <marcusday117@gmail.com>
2017-03-23 16:11:42 +1100

    Placed in piston sprite pieces
    Created Piston script to test out
    
    Created animation folder and created piston animation

---
+++ HardEndlessRunner/Assets/Scripts/Mechanisms/Piston.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piston : MonoBehaviour
+{
+    bool MoveLeft;
+
+     Transform headTrans;
+     Transform rodTrans;
+
+    // Use this for initialization
+    void Awake()
+    {
+        headTrans = transform.GetChild(0);
+        rodTrans = transform.GetChild(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        rodTrans.transform.Rotate(0, 0, 2.5f);
+    }
+}
