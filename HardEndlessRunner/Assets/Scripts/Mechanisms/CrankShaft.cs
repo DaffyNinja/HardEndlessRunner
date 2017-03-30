95a46532049e980cdded06a303d8218ed75e4a41
DaffyNinja <marcusday117@gmail.com>
2017-03-30 11:17:24 +1100

    Placed in test sprite of crankshaft and made crankshaft script to test

---
+++ HardEndlessRunner/Assets/Scripts/Mechanisms/CrankShaft.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrankShaft : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+		
+	}
+}
---
+++ HardEndlessRunner/Assets/Scripts/MovementUpDown.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementUpDown : MonoBehaviour {
+
+    public bool isUp;
+    public float upSpeed;
+
+    public bool isDown;
+    public float downSpeed;           
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
