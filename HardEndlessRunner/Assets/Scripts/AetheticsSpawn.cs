4c5f513df3054497da27dee6927ea54e15c9e4fc
DaffyNinja <marcusday117@gmail.com>
2017-03-31 14:49:45 +1100

    Changed sprites wall on saw

---
+++ HardEndlessRunner/Assets/Scripts/AetheticsSpawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AetheticsSpawn : MonoBehaviour {
+
+    public List<GameObject> aestheticsList;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        Instantiate(aestheticsList[Random.Range(0,aestheticsList.Count)], transform.position, Quaternion.identity); 		
+	}
+}
