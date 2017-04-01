6f39e03f04efcb18d3f20949ee0c0f4d71b2793e
DaffyNinja <marcusday117@gmail.com>
2017-04-01 22:15:34 +1100

    Changed Spike tracks to have brick and metal walls art
    Added gears to grounded saws track
    
    Changed name of tack placeholder folder to tracks
    
    Added obstacles folder to prefabs folder
    
    Made Crankshaft work with inoutspikes object to change rotation
    
    Added brick and metal walls sprites to more tracks
    
    Changed some saw sprites

--- HardEndlessRunner/Assets/Scripts/Mechanisms/CrankShaft.cs
+++ HardEndlessRunner/Assets/Scripts/Mechanisms/CrankShaft.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class CrankShaft : MonoBehaviour {
 
+    public InOutSpikes spikeCS;
+
 	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+	void Awake ()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+        if (spikeCS.isActive)
         {
             transform.localRotation = Quaternion.Euler(0, 180, 0);
         }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        else
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
 		
 	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //if (Input.GetKeyDown(KeyCode.Alpha1))
+        //{
+        //    transform.localRotation = Quaternion.Euler(0, 180, 0);
+        //}
+        //else if (Input.GetKeyDown(KeyCode.Alpha2))
+        //{
+        //    transform.localRotation = Quaternion.Euler(0, 0, 0);
+        //}
+
+        if (spikeCS.isActive)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+    }
 }
