c657aba24cd2477f2757e75a8f2bf2f2532bb491
DaffyNinja <marcusday117@gmail.com>
2017-04-05 11:59:26 +1000

    Created spawn rate chance for Aesthetic spawn

--- HardEndlessRunner/Assets/Scripts/AetheticsSpawn.cs
+++ HardEndlessRunner/Assets/Scripts/AetheticsSpawn.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class AetheticsSpawn : MonoBehaviour {
 
-    public List<GameObject> aestheticsList;
+    public int spawnMaxChance;
+    [Space(5)]
+    public List<GameObject> aestheticsObjectsList;
+    //[Space(5)]
+    //public List<Texture2D> aestheticsSpritesList;
 
-	// Use this for initialization
-	void Awake ()
+    // Use this for initialization
+    void Awake()
     {
-        Instantiate(aestheticsList[Random.Range(0,aestheticsList.Count)], transform.position, Quaternion.identity); 		
-	}
+        int ranNum = Random.Range(0, spawnMaxChance + 1);
+
+        if (ranNum == spawnMaxChance)
+        {
+           Instantiate(aestheticsObjectsList[Random.Range(0, aestheticsObjectsList.Count)], transform.position, Quaternion.identity);
+            
+            //else // Sprites
+            //{
+            //    Instantiate(aestheticsSpritesList[Random.Range(0, aestheticsSpritesList.Count)], transform.position, Quaternion.identity);
+            //}
+        }
+
+    }
 }
